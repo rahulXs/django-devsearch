32c7a4cb1f59ffdb248230ffbd966bc0cff83582
Rahul <rahul.sharma201@outlook.com>
2021-08-11 18:09:23 +0530

    Installed UI theme

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--wkh@29zw^xlcmf7r4w(7f%w6z*)_%_3ql003=r83f*5s+xv5n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,25 @@
 
 from django.forms import ModelForm
+from django import forms
+
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description', 'featured_image', 'demo_link', 'source_link', 'tags']
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    #override init
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        #self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add a title'})
+        #self.fields['description'].widget.attrs.update({'class':'input', 'placeholder': 'Add description'})
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
