1048a9a4bbe46a8afdc316223ed136b033feaccb
Rahul <rahul.sharma201@outlook.com>
2021-08-12 22:12:07 +0530

    Add login_required and flash messages

--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .models import Profile
 
 # Create your views here.
 def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -13,7 +17,7 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username=username, password=password)
 
@@ -21,12 +25,12 @@ def loginUser(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username OR password is incorrect')
-
+            messages.error(request, 'Username OR password is incorrect')
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.info(request, 'User was logged out!')
     return redirect('login')
 
 def profiles(request):
