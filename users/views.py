192449d8321699cd6bffa8acdac758b565e297b4
Rahul <rahul.sharma201@outlook.com>
2021-08-13 10:38:31 +0530

    Add custom user registration form

--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def loginUser(request):
@@ -36,10 +36,10 @@ def logoutUser(request):
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
