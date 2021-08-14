03f3475320092ba650c53d236e5f76daf213d0d9
Rahul <rahul.sharma201@outlook.com>
2021-08-14 10:51:54 +0530

    Add skill create and update

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('profile/<str:pk>', views.userProfiles, name="user-profile"),
     path('account/', views.userAccount, name="account"),
     path('edit-account/', views.editAccount, name="edit-account"),
+
+    path('create-skill/', views.createSkill, name="create-skill"),
+    path('update-skill/<str:pk>/', views.updateSkill, name="update-skill"),
 ]
