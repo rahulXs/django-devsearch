b37862e8aadb878798f6f8b6f23a94cf998951bb
Rahul <rahul.sharma201@outlook.com>
2021-08-14 14:46:11 +0530

    Add search for projects

--- projects/views.py
+++ projects/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
+from .utils import searchProjects
 
 # Create your views here.
 def projects(request):
-    projects = Project.objects.all()
-    context = {'projects': projects}
+    projects, search_query = searchProjects(request)
+    context = {'projects': projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
