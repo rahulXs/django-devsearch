7950914d6eb07e1ad95f67952663f8de8509ba81
Rahul <rahul.sharma201@outlook.com>
2021-08-14 14:31:29 +0530

    Add search for user profiles

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 
 from .models import Profile
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .utils import searchProfiles
 
 # Create your views here.
 def loginUser(request):
@@ -59,8 +60,8 @@ def registerUser(request):
     return render(request, 'users/login_register.html', context)
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = searchProfiles(request)
+    context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
 def userProfiles(request, pk):
