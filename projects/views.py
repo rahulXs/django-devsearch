cf846fa4c7f896c0b5356325b74d31adb5d73928
Rahul <rahul.sharma201@outlook.com>
2021-08-26 21:56:13 +0530

    Add pagination for projects

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.core import paginator
+
 from .models import Project
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 
 # Create your views here.
 def projects(request):
     projects, search_query = searchProjects(request)
-    context = {'projects': projects, 'search_query': search_query}
+    custom_range, projects = paginateProjects(request, projects, 3)
+
+    context = {'projects': projects, 'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
